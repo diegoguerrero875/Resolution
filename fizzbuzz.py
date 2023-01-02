864b369326cf612df1c4364d19eeb3ab1490575d
Yogui <dguerrero875@gmail.com>
2023-01-01 21:33:42 -0800

    fizzbuzz with python

---
+++ fizzbuzz.py
@@ -0,0 +1,10 @@
+#FizzBuzz by DiegoGG
+def fizzbuzz(n):
+    if n % 3 == 0 and n % 5 == 0:
+        print("fizzbuzz")
+    elif n % 3 == 0:
+        print("fizz")
+    elif n % 5 == 0:
+        print("buzz")
+
+fizzbuzz(30)
