d8c372fb599cbff0ddbe8051a974276c8a031971
Yogui <dguerrero875@gmail.com>
2023-01-04 11:32:58 -0800

    more vim commands

---
+++ day4.py
@@ -0,0 +1,7 @@
+#more vim learing things
+#crtl + [ = enter normal mode
+#C = Delete from cursor to end of the line
+#w = go to begginig of next word
+#b = backward to the beggining of word
+#e = forward to the end of the word
+# all of this 3 commands in caps work with punctuation and long words
