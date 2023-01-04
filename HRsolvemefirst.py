bd7d97968ee1635266db42e858c7ed06dfe81032
Yogui <dguerrero875@gmail.com>
2023-01-03 21:54:05 -0800

    hacker rank p1

---
+++ HRsolvemefirst.py
@@ -0,0 +1,13 @@
+#Hacker rank solveMeFirst problem
+
+def solveMeFirst(a,b):
+    # Hint: Type return a+b below
+    c = a + b
+    return(c)
+
+
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1,num2)
+print(res)
+
